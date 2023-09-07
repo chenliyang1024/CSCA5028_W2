9bff92ed6a157de18f7b68c035775aa55876f184
Danny Chen <chenliyang1024@gmail.com>
2023-09-07 14:44:07 +0800

    F

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --bind 0.0.0.0:$PORT src.app:app
+web: gunicorn src.app:app
--- src/app.py
+++ src/app.py
@@ -17,6 +17,3 @@ def main():
 def echo_input():
     input_text = request.form.get("user_input", "")
     return "You entered: " + input_text
-
-if __name__ == '__main__':
-    app.run()
